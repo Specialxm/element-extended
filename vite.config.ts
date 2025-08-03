6a23bb2f189a0f6d1b10d4b33ba8a78b905c565e
Special英 <491882353@qq.com>
2025-08-04 00:19:53 +0800

    调整文件结构

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  resolve: {},
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./apps/web', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'apps/web/main.ts',
+    },
+  },
 })
